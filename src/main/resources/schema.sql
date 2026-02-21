81364e58b01690173571e8a304b3ae1ae74da136
elnehc <piccocube@gmail.com>
2026-02-21 00:04:19 -0500

    updated schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,37 @@
-CREATE TABLE User{
+CREATE TABLE User (
+    user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
+    phone_number varchar(20) NOT NULL,
+    address varchar(255) NOT NULL
+)
 
-    }
+CREATE TABLE Order (
+    order_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_number varchar(20) NOT NULL UNIQUE,
+    user_id int NOT NULL,
+    order_date datetime NOT NULL,
+    order_status ENUM('PLACED','CANCELLED','SHIPPED','DELIVERED','RETURNED') NOT NULL,
+    total_amount decimal(10, 2) NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+)
+
+CREATE TABLE Order_Detail (
+    order_detail_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id int NOT NULL,
+    product_id int NOT NULL,
+    quantity int NOT NULL,
+    unit_price decimal(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Order(order_id),
+    FOREIGN KEY (product_id) REFERENCES Product(product_id)
+)
+
+CREATE TABLE Product (
+    product_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    brand varchar(50) NOT NULL,
+    category varchar(50) NOT NULL,
+    description text,
+)
